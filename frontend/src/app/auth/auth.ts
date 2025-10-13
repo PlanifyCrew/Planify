56dbf34c40d99bf078c86965063795d93d7359c0
8ubb1egum <beguemoez2005@gmail.com>
2025-10-13 02:51:02 +0200

    fronted struktur angelegt

---
+++ frontend/src/app/auth/auth.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-auth',
+  standalone: true,
+  imports: [],
+  templateUrl: './auth.html',
+  styleUrls: ['./auth.css']
+})
+export class Auth {
+
+}
---
+++ frontend/src/app/home/event-dialog/event-dialog.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-event-dialog',
+  standalone: true,
+  imports: [],
+  templateUrl: './event-dialog.html',
+  styleUrls: ['./event-dialog.css']
+})
+export class EventDialog {
+
+}
