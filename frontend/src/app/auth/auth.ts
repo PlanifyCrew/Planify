2a07d7dfb3b18a4c50b3556907ac016357c70f95
RobertKraus <128461290+RobertKraus@users.noreply.github.com>
2025-10-14 17:31:25 +0200

    TS und erster BFF Test mit Spring Boot

--- frontend/src/app/auth/auth.ts
+++ frontend/src/app/auth/auth.ts
@@ -1,12 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TaskService } from '../../data/task-service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-auth',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './auth.html',
   styleUrls: ['./auth.css']
 })
-export class Auth {
 
+export class Auth implements OnInit {
+  email: string = '';
+  password: string = '';
+  private token: any;
+
+  constructor(private taskService: TaskService) { }
+  ngOnInit() { }
+
+  login(): void {
+    alert('Logging in ' + this.email + ' password ' + this.password);
+
+    let userLogin = {
+      email: this.email,
+      password : this.password
+    }
+
+    this.taskService.postUserLogIn(userLogin).subscribe(
+      data => {
+        this.token = data;
+        console.log(this.token);
+        localStorage.setItem('dummy_token', this.token.token);
+      },
+      err => console.log('Could not reach heroku.'),
+      () => console.log('Login complete.')
+    );
+  }
 }
