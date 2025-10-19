697759a7445c405762d3b4da92a395808d2e6057
8ubb1egum <beguemoez2005@gmail.com>
2025-10-20 00:51:15 +0200

    Kalendercomponente hinzugefügt

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,15 @@
+
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-	{
-		path: '',
-		loadComponent: () => import('./home/home').then(m => m.Home)
-	},
-	{
-		path: 'auth',
-		loadComponent: () => import('./auth/auth').then(m => m.Auth)
-	},
-	// fallback to home
-	{ path: '**', redirectTo: '', pathMatch: 'full' }
+  {
+    path: '',
+    loadComponent: () => import('./home/home').then(m => m.HomeComponent)
+  },
+  {
+    path: 'auth',
+    loadComponent: () => import('./auth/auth').then(m => m.Auth)
+  },
+  { path: '**', redirectTo: '', pathMatch: 'full' }
 ];
+
