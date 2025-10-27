ddd49f8ed1b6b3f0418b6c98e8ff1d31a5124ab3
RobertKraus <128461290+RobertKraus@users.noreply.github.com>
2025-10-27 17:27:52 +0100

    Node Server

---
+++ SPA/server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+// Statische Dateien aus dem Build-Ordner
+app.use(express.static(path.join(__dirname, 'dist/frontend')));
+
+// Alle anderen Routen auf index.html umleiten
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist/frontend', 'index.html'));
+});
+
+// Heroku-Port nutzen
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
