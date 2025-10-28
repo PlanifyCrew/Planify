6b94b1abb6f5a3e2c6271e5a0ea0098330c4f7e7
RobertKraus <128461290+RobertKraus@users.noreply.github.com>
2025-10-28 12:06:09 +0100

    Node Server Express Version und richtiger root-Pfad

--- SPA/server.js
+++ SPA/server.js
@@ -2,13 +2,17 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-const staticPath = path.join(__dirname, 'dist/tasksspa/browser');
+// Absoluter Pfad zum SPA Build
+const staticPath = path.join(__dirname, 'dist', 'frontend');
+
+// Statische Dateien bereitstellen
 app.use(express.static(staticPath));
 
-// Express 5.x: param name muss angegeben werden
-app.all("/*splat", (req, res) => {
-  res.sendFile("index.html", { root: "dist/tasksspa/browser/" });
+// SPA-Fallback (Express v5)
+app.all('/*splat', (req, res) => {
+  res.sendFile('index.html', { root: staticPath });
 });
 
+// Port starten
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
