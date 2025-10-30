15c24ef83d9292f175ae9e5ea265dec740131406
RobertKraus <128461290+RobertKraus@users.noreply.github.com>
2025-10-30 16:23:23 +0100

    SPA Heroku Test

---
+++ SPA/src/data/graphql-service.spec.ts
@@ -0,0 +1,7 @@
+import { GraphqlService } from './graphql-service.ts';
+
+describe('GraphqlService', () => {
+  it('should create an instance', () => {
+    expect(new GraphqlService()).toBeTruthy();
+  });
+});
