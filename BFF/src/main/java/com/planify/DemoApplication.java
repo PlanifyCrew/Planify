5f1b0620815eaa8f31c358baf20c9dc5bdded4dc
RobertKraus <128461290+RobertKraus@users.noreply.github.com>
2025-11-03 03:15:51 +0100

    BFF Bug und neu Build

--- BFF/src/main/java/com/planify/DemoApplication.java
+++ BFF/src/main/java/com/planify/DemoApplication.java
@@ -3,7 +3,7 @@ package com.planify;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.planify")
 public class DemoApplication {
 
 	public static void main(String[] args) {
